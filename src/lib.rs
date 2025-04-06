1418c0346fce16f54afc0280f7fac11f78bc25e5
TeenCoder159 <no1umang@gmail.com>
2025-04-07 01:12:14 +0800

    Add search function

--- src/lib.rs
+++ src/lib.rs
@@ -28,4 +28,49 @@ impl Data {
         self.autocorrect = autocorrect;
         self
     }
+
+    /// Search through the provided context (of the Data struct)
+    /// Currently, doesn't support multi word search (ex: searching for an instance of "two words")
+    pub fn search(self, search_target: String, margin: u8) -> Option<(Vec<String>, u32)> {
+        let target = search_target.as_str().trim();
+        let mut target_counter = 0;
+        let mut result = Vec::new();
+
+        if !self.content.contains(target) {
+            return None;
+        }
+
+        for line in self.content.lines() {
+            if line.contains(target) {
+                //
+
+                let word_count = line.split_whitespace().count();
+                let min_word_count = (2 * margin) + 1;
+
+                //let mut temp_vec = vec![];
+                //let mut temp_vec_counter = 0;
+                //let mut word_pos: i32;
+
+                for word in line.split_whitespace() {
+                    if word.contains(target) {
+                        target_counter += 1
+                    }
+                    result.push(line.to_string());
+                    if word_count <= min_word_count as usize {
+
+                        // } else {
+                        //     temp_vec.push(word);
+
+                        //     if word.contains(target) {
+                        //         word_pos = temp_vec_counter;
+                        //     }
+
+                        //     temp_vec_counter += 1;
+                    }
+                }
+            }
+        }
+
+        Some((result, target_counter))
+    }
 }
