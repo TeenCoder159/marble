b36454363b1b7b9cfb7b801f9bec6a6ce96d19d3
TeenCoder159 <no1umang@gmail.com>
2025-04-06 23:15:53 +0800

    Add basic setup functions

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,28 @@
+pub struct Data {
+    content: String,
+    case_sens: bool,
+    autocorrect: bool,
+}
 
+impl Data {
+    /// Creates the default implementation of the Data impl with autocorrect and case_sensitivity being on
+    fn new(content: String) -> Self {
+        Self {
+            content,
+            case_sens: true,
+            autocorrect: true,
+        }
+    }
+
+    /// Sets the case sensitivity to the desired setting
+    fn case_sens(&mut self, sensitivity: bool) -> &mut Self {
+        self.case_sens = sensitivity;
+        self
+    }
+
+    /// Sets autocorrect to the desired setting
+    fn autocorrect(&mut self, autocorrect: bool) -> &mut Self {
+        self.autocorrect = autocorrect;
+        self
+    }
+}
